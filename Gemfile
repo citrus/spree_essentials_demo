21848ff1df5c972f43aadfe0ab81532c8f113aba
Spencer Steffen <spencer@citrusme.com>
2011-04-04 15:35:31 -0700

    initial commit

---
+++ Gemfile
@@ -0,0 +1,14 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.5'
+gem 'aws-s3'
+gem 'sqlite3'
+
+gem 'spree', '0.50.0'
+gem 'spree_heroku', :git => 'git://github.com/paxer/spree-heroku.git'
+
+gem 'spree_essentials', :path => "~/RoR/gems"
+
+
+#:path => "~/RoR/gems"
+#:git => 'git://github.com/citrus/spree_essentials.git'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SpreeEssentialsDemo::Application.config.secret_token = '519f9c39e2c13c3d16c006d98bb05d6ee42ef57e35109e8c2615382c0ab35c3ed5f38b9b2c9c4adfbcce4ed8aebca0a77488e9564cccf63df1252454ca25d17f'
